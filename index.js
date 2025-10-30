89338ed105eed7d55fe75d88de1b1cdfa2e94a90
Ridwan Olojede <olojederidwan@gmail.com>
2025-10-30 00:39:51 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,91 @@
+console.log("Ridwan Olojede");
+console.log("I love Abisoye");
+20;
+100;
+
+("i love you");
+`fuck`;
+
+true;
+false;
+
+// How to create variable
+let numberOfAge = 20;
+let nameOfStudent = "Ridwan";
+let hasTheStudentPassTheExam = true;
+
+console.log(numberOfAge);
+console.log(nameOfStudent);
+console.log(hasTheStudentPassTheExam);
+
+// REASSIGNMENT VARIABLE
+let ourVariable = 100;
+
+ourVariable = 300;
+ourVariable = 33;
+
+console.log(ourVariable);
+
+let myNumber = 10;
+myNumber = 33;
+
+console.log(myNumber);
+
+// OPERATION
+let result = 1 + 1;
+
+console.log(result);
+
+let oldBlanace = 100;
+let income = 300;
+
+let newBalance = oldBlanace + income;
+
+console.log(newBalance);
+
+// SUB
+let oldMoney = 122;
+let newMoney = 20;
+
+let currentMoney = oldMoney - newMoney;
+
+console.log(currentMoney);
+
+// multipy
+let multiOldMoney = 2;
+let multiNewMoney = 4;
+
+let rentMoney = multiNewMoney * multiOldMoney;
+
+console.log(rentMoney);
+
+// DIVI
+let diviNew = 4;
+let diviOld = 2;
+
+let diviCurreny = diviNew / diviOld;
+
+console.log(diviCurreny);
+
+// MUDULOS (REMAINDER)
+
+let mudNew = 10;
+let mudOld = 3;
+
+let mudCurrency = mudNew % mudOld;
+
+console.log(mudCurrency);
+
+// EXPONENTIATION (RAISE TO POWER)
+let powerOld = 2;
+let powerNew = 3;
+
+let powerCurrent = powerNew ** powerOld;
+
+console.log(powerCurrent);
+
+let firstOld = "love";
+firstOld = 4;
+firstOld = true;
+
+console.log(firstOld);
